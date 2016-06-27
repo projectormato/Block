eda13edd865577007084a9112b7a4ed30bfa0eea
yuuki <yuuki0xff@gmail.com>
2016-06-28 06:09:59 +0900

    remove unused code

--- BlockGame/Cursor.java
+++ BlockGame/Cursor.java
@@ -22,32 +22,6 @@ public class Cursor extends BaseActor {
         this.goal = goal;
         this.ball = ball;
         this.blocks = blocks;
-
-        List<String> keys = new ArrayList<>();
-        keys.add("up");
-        keys.add("down");
-        keys.add("left");
-        keys.add("right");
-
-        setListenKeys(keys);
-    }
-
-    @Override
-    public void onKeyHolding(String key) {
-        // Add your action code here.
-        if (Greenfoot.isKeyDown("up")) {
-            setLocation(getX(), getY() - 1);
-        } else if (Greenfoot.isKeyDown("down")) {
-            setLocation(getX(), getY() + 1);
-        } else if (Greenfoot.isKeyDown("left")) {
-            setLocation(getX() - 1, getY());
-        } else if (Greenfoot.isKeyDown("right")) {
-            setLocation(getX() + 1, getY());
-        }
-        if (isTouching(Goal.class)) {
-            System.out.println("touch!");
-            setLocation(getX() + 100, getY() - 100);
-        }
     }
 
     @Override
