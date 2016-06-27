a081145b51dbc7326fccf93bf456c9fc17482b64
yuuki <yuuki0xff@gmail.com>
2016-06-28 04:23:22 +0900

    format

--- BlockGame/Cursor.java
+++ BlockGame/Cursor.java
@@ -1,11 +1,12 @@
+
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Write a description of class Cursor here.
- * 
- * @author (your name) 
+ *
+ * @author (your name)
  * @version (a version number or a date)
  */
 public class Cursor extends BaseActor {
@@ -29,23 +30,22 @@ public class Cursor extends BaseActor {
     }
 
     @Override
-    public void onKeyHolding(String key)
-    {
+    public void onKeyHolding(String key) {
         // Add your action code here.
-        if(Greenfoot.isKeyDown("up")){
-            setLocation(getX(), getY() -1);
-        }    else if(Greenfoot.isKeyDown("down")){
-            setLocation(getX(), getY() +1);
-        }else if(Greenfoot.isKeyDown("left")){
-            setLocation(getX() -1, getY());
-        }else if(Greenfoot.isKeyDown("right")){
-            setLocation(getX() +1, getY());
+        if (Greenfoot.isKeyDown("up")) {
+            setLocation(getX(), getY() - 1);
+        } else if (Greenfoot.isKeyDown("down")) {
+            setLocation(getX(), getY() + 1);
+        } else if (Greenfoot.isKeyDown("left")) {
+            setLocation(getX() - 1, getY());
+        } else if (Greenfoot.isKeyDown("right")) {
+            setLocation(getX() + 1, getY());
         }
-        if(isTouching(Goal.class)){
+        if (isTouching(Goal.class)) {
             System.out.println("touch!");
-            setLocation(getX() +100, getY() -100);
+            setLocation(getX() + 100, getY() - 100);
         }
-    }    
+    }
 
     @Override
     public void onMouseMoved(MouseInfo mouse) {
--- BlockGame/Goal.java
+++ BlockGame/Goal.java
@@ -3,8 +3,8 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
  * Write a description of class Goal here.
- * 
- * @author (your name) 
+ *
+ * @author (your name)
  * @version (a version number or a date)
  */
 public class Goal extends BaseActor {
