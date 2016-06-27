1218444d0cbd6b76ba1173dbf4041c9715c33e3d
yuuki <yuuki0xff@gmail.com>
2016-06-28 04:43:50 +0900

    clean up

--- BlockGame/Ball.java
+++ BlockGame/Ball.java
@@ -1,11 +1,11 @@
+
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
  * Write a description of class Ball here.
- * 
- * @author (your name) 
+ *
+ * @author (your name)
  * @version (a version number or a date)
  */
-public class Ball extends BaseActor
-{
+public class Ball extends BaseActor {
 }
