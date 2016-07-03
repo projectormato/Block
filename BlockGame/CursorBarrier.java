e38b6d332558b102582586db1faa2ec0b4cf5d22
yuuki <yuuki0xff@gmail.com>
2016-07-04 00:07:29 +0900

    ボールの反射角度を衝突地点に応じて変化させるようにした

--- BlockGame/CursorBarrier.java
+++ BlockGame/CursorBarrier.java
@@ -2,6 +2,7 @@
 public class CursorBarrier extends BaseActor {
 
     private Ball ball;
+    private int k = 5;
 
     public CursorBarrier(Ball ball) {
         this.ball = ball;
@@ -11,7 +12,44 @@ public class CursorBarrier extends BaseActor {
     public void tick() {
         super.tick();
         if (isTouching(ball.getClass())) {
-            ball.setRotation(getRotation());
+            // ボールを反射させる
+            double dx, dy;
+            double dangle;          // バリアからみた時のボールの方角
+            double incidenceAngle;  // 入射角
+            double reflectionAngle; // 反射角
+
+            // 裏側から衝突したボールには処理を行わない
+            if (Math.abs(ball.getRotation() - getRotation()) <= 90) {
+                return;
+            }
+
+            dx = ball.getX() - getX();
+            dy = ball.getY() - getY();
+            dangle = normalizeRelativeAngle(Math.toDegrees(Math.atan2(dy, dx)) - getRotation());
+
+            incidenceAngle = 180 + ball.getRotation() - getRotation();
+            reflectionAngle = -incidenceAngle;
+            // 衝突地点に応じて、反射角を調整
+            reflectionAngle += dangle / k;
+
+            ball.setRotation(getRotation() + (int) reflectionAngle);
         }
+    }
+
+    /**
+     * 角度(degree)を正規化する
+     *
+     * @param angle (degree)
+     * @return -180 ~ 180 (degree)
+     */
+    private double normalizeRelativeAngle(double angle) {
+        angle %= 360;
+        if (angle < 0) {
+            angle = 360 + angle;
+        }
+        if (180 < angle) {
+            angle = -360 + angle;
+        }
+        return angle;
     }
 }
