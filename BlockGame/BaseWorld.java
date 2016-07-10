e4c34a76a1ae2a6d4ab6e2e32557eba8477bcba8
yuuki <yuuki0xff@gmail.com>
2016-07-11 01:50:04 +0900

    format

--- BlockGame/BaseWorld.java
+++ BlockGame/BaseWorld.java
@@ -22,7 +22,7 @@ public class BaseWorld extends World implements EventHandler {
 
     public BaseWorld() {
         // Create a new world with 1200x800 cells with a cell size of 1x1 pixels.
-        super(Config.WORLD_WIDTH , Config.WORLD_HEIGHT, 1);
+        super(Config.WORLD_WIDTH, Config.WORLD_HEIGHT, 1);
         setPaintOrder(MessageBox.class, BaseActor.class);
     }
 
