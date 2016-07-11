d568144a7024917bca4e26e34ab0ed3b09da9978
yuuki <yuuki0xff@gmail.com>
2016-07-11 19:58:44 +0900

    BlinkMessageBox: 画像を事前にキャッシュするようにした

--- BlockGame/BlinkMessageBox.java
+++ BlockGame/BlinkMessageBox.java
@@ -1,4 +1,5 @@
 
+import greenfoot.*;
 import java.awt.Color;
 
 /**
@@ -7,9 +8,10 @@ import java.awt.Color;
  */
 public class BlinkMessageBox extends MessageBox {
 
-    private double blinkInterbal = 100;
-    private double radian = 0;
+    private int blinkInterbal = 100;
     private Color originalFontColor;
+    private GreenfootImage[] imageCache;
+    private int count = 0;
 
     public BlinkMessageBox(String msgConfigName, int width, int height) {
         super(msgConfigName, width, height);
@@ -22,19 +24,35 @@ public class BlinkMessageBox extends MessageBox {
         backgroundColor = new Color(0, true);
     }
 
+    /**
+     * 画像のキャッシュを作成する
+     */
+    public void createImageCache() {
+        imageCache = new GreenfootImage[blinkInterbal];
+        double radian = 0;
+        for (int i = 0; i < blinkInterbal; i++) {
+            // Alpha値を更新
+            int rgba = originalFontColor.getRGB() & 0xffffff;
+            rgba |= (int) Math.abs(Math.cos(radian) * originalFontColor.getAlpha()) << 24;
+            fontColor = new Color(rgba, true);
+            radian += Math.PI * 2 / blinkInterbal;
+
+            // 再描画してキャッシュに追加
+            draw();
+            imageCache[i] = new GreenfootImage(getImage());
+        }
+    }
+
     @Override
     public void tick() {
         if (getActorStatus() != ActorStatus.ALIVE) {
             return;
         }
+        if (imageCache == null) {
+            createImageCache();
+        }
 
-        // Alpha値を更新
-        int rgba = originalFontColor.getRGB() & 0xffffff;
-        rgba |= (int) Math.abs(Math.cos(radian) * originalFontColor.getAlpha()) << 24;
-        fontColor = new Color(rgba, true);
-        radian += Math.PI * 2 / blinkInterbal;
-
-        // 再描画
-        draw();
+        setImage(imageCache[count % blinkInterbal]);
+        count++;
     }
 }
