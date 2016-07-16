23eeeec033650110cf7e0ccaf1c8bf935dd72032
yuuki <yuuki0xff@gmail.com>
2016-07-16 21:47:08 +0900

    add Overlay class

---
+++ BlockGame/Overlay.java
@@ -0,0 +1,25 @@
+
+import java.awt.Color;
+import greenfoot.GreenfootImage;
+
+/**
+ * メッセージボックスの半透明の背景
+ */
+public class Overlay extends BaseActor {
+
+    GreenfootImage img;
+
+    /**
+     * 指定したサイズ・色の背景を作成する
+     *
+     * @param width
+     * @param height
+     * @param color 背景色
+     */
+    public Overlay(int width, int height, Color color) {
+        img = new GreenfootImage(width, height);
+        img.setColor(color);
+        img.fill();
+        setImage(img);
+    }
+}
