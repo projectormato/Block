722a88844244eb7374903f3f824f301acc7d9e42
yuuki <yuuki0xff@gmail.com>
2016-07-26 00:39:40 +0900

    ImageButton: アニメーション対応(途中)

--- BlockGame/ImageButton.java
+++ BlockGame/ImageButton.java
@@ -1,6 +1,9 @@
 
 import greenfoot.GreenfootImage;
+import greenfoot.MouseInfo;
 import greenfoot.World;
+import java.awt.Color;
+import java.awt.Graphics2D;
 
 /**
  * 画像ボタン
@@ -9,7 +12,7 @@ import greenfoot.World;
  */
 public class ImageButton extends Button {
 
-    private int count = 0;
+    private boolean isMouseHovering = false;
 
     /**
      * keyに対応する画像のボタンを作成する。
@@ -21,17 +24,51 @@ public class ImageButton extends Button {
     }
 
     @Override
-    public void tick() {
-        // アニメーション
-    }
-
-    @Override
     public void addedToWorld(World world) {
         super.addedToWorld(world);
 
         normalImg = Config.getImage(this, key);
         setImage(normalImg);
-        pressingImg = new GreenfootImage(normalImg);
-        // TODO: ボタンが押された時の画像を生成する処理を入れる
+    }
+
+    @Override
+    public void animate(int step) {
+        super.animate(step);
+        System.out.println("ImgBtn: update");
+
+        // 丸みを帯びた四角形を描くために、大きめの画像サイズを確保する
+        int width, height;
+        final int diameter = normalImg.getHeight();
+        final int radius = diameter / 2;
+        width = normalImg.getWidth() + diameter;
+        height = normalImg.getHeight();
+        GreenfootImage img = new GreenfootImage(width, height);
+        Graphics2D g = img.getAwtImage().createGraphics();
+
+        // 左右が丸みを帯びた四角形を描く
+        g.setColor(Color.DARK_GRAY);
+        g.fillOval(0, 0, diameter, diameter);
+        g.fillOval(width - diameter, 0, diameter, diameter);
+        g.fillRect(radius, 0, width - diameter, height);
+        g.dispose();
+
+        // 通常時の画像を重ねて描画する
+        img.drawImage(normalImg, radius, 0);
+        Utils.updateMaxAlpha(img, maxAlpha);
+        setImage(img);
+    }
+
+    @Override
+    public void onMouseIn(MouseInfo mouse) {
+        super.onMouseIn(mouse);
+        isMouseHovering = true;
+        startAnimation();
+    }
+
+    @Override
+    public void onMouseOut(MouseInfo mouse) {
+        super.onMouseOut(mouse);
+        isMouseHovering = false;
+        startAnimation();
     }
 }
