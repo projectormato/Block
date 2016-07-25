7e6ac13119be6e0ae969406c3a83f5ddca5cc49e
yuuki <yuuki0xff@gmail.com>
2016-07-26 01:11:36 +0900

    Button側にメソッドを移動

--- BlockGame/Button.java
+++ BlockGame/Button.java
@@ -1,5 +1,7 @@
 
 import greenfoot.*;
+import java.awt.Color;
+import java.awt.Graphics2D;
 
 /**
  * アニメーションをするボタン。
@@ -16,6 +18,11 @@ public abstract class Button extends BaseAnimationActor {
     protected String key;
     protected int maxAlpha = 0xff;
 
+    // addedToWorldの描画で、背景がnormalColorで描画されるようにするため、trueにしている。
+    protected boolean isMouseHovering = true;
+    protected Color normalColor = Color.DARK_GRAY;
+    protected Color hoveringColor = Color.GRAY;
+
     public Button() {
         onMouseInSound = Config.getSound(this.getClass(), "mouseIn");
         onMouseClickedSound = Config.getSound(this.getClass(), "mouseClicked");
@@ -30,6 +37,63 @@ public abstract class Button extends BaseAnimationActor {
         this.maxAlpha = maxAlpha;
     }
 
+    /**
+     * 画像を描画する。
+     *
+     * @param step アニメーションの進行状況
+     */
+    protected void draw(int step) {
+        // 丸みを帯びた四角形を描くために、大きめの画像サイズを確保する
+        int width, height;
+        final int diameter = normalImg.getHeight();
+        final int radius = diameter / 2;
+        width = normalImg.getWidth() + diameter;
+        height = normalImg.getHeight();
+        GreenfootImage img = new GreenfootImage(width, height);
+        Graphics2D g = img.getAwtImage().createGraphics();
+
+        // 左右が丸みを帯びた四角形を描く
+        g.setColor(getColor(step));
+        g.fillOval(0, 0, diameter, diameter);
+        g.fillOval(width - diameter, 0, diameter, diameter);
+        g.fillRect(radius, 0, width - diameter, height);
+        g.dispose();
+
+        // 通常時の画像を重ねて描画する
+        img.drawImage(normalImg, radius, 0);
+        Utils.updateMaxAlpha(img, maxAlpha);
+        setImage(img);
+    }
+
+    protected void draw() {
+        draw(0);
+    }
+
+    /**
+     * 現在のアニメーションの状態に合わせた、ボタンの背景にある丸みを帯びた四角形の色を返す
+     *
+     * @param step
+     * @return
+     */
+    protected Color getColor(int step) {
+        int r, g, b;
+        if (isMouseHovering) {
+            // normal -> hovering
+            r = normalColor.getRed() * (ANIME_STEP - step) + hoveringColor.getRed() * step;
+            g = normalColor.getGreen() * (ANIME_STEP - step) + hoveringColor.getGreen() * step;
+            b = normalColor.getBlue() * (ANIME_STEP - step) + hoveringColor.getBlue() * step;
+        } else {
+            // hovering -> normal
+            r = normalColor.getRed() * step + hoveringColor.getRed() * (ANIME_STEP - step);
+            g = normalColor.getGreen() * step + hoveringColor.getGreen() * (ANIME_STEP - step);
+            b = normalColor.getBlue() * step + hoveringColor.getBlue() * (ANIME_STEP - step);
+        }
+        r /= ANIME_STEP;
+        g /= ANIME_STEP;
+        b /= ANIME_STEP;
+        return new Color(r, g, b);
+    }
+
     @Override
     public void onMouseIn(MouseInfo mouse) {
         super.onMouseIn(mouse);
@@ -39,6 +103,15 @@ public abstract class Button extends BaseAnimationActor {
         if (Config.getBoolean("enableSoundEffect") && onMouseInSound != null) {
             onMouseInSound.play();
         }
+        isMouseHovering = true;
+        startAnimation();
+    }
+
+    @Override
+    public void onMouseOut(MouseInfo mouse) {
+        super.onMouseOut(mouse);
+        isMouseHovering = false;
+        startAnimation();
     }
 
     @Override
--- BlockGame/ImageButton.java
+++ BlockGame/ImageButton.java
@@ -1,9 +1,5 @@
 
-import greenfoot.GreenfootImage;
-import greenfoot.MouseInfo;
 import greenfoot.World;
-import java.awt.Color;
-import java.awt.Graphics2D;
 
 /**
  * 画像ボタン
@@ -11,11 +7,6 @@ import java.awt.Graphics2D;
  * @author yuuki0xff
  */
 public class ImageButton extends Button {
-
-    // addedToWorldの描画で、背景がnormalColorで描画されるようにするため、trueにしている。
-    private boolean isMouseHovering = true;
-    private Color normalColor = Color.DARK_GRAY;
-    private Color hoveringColor = Color.GRAY;
 
     /**
      * keyに対応する画像のボタンを作成する。
@@ -41,79 +32,7 @@ public class ImageButton extends Button {
             stopAnimation();
             return;
         }
-        System.out.println("ImgBtn: update");
-
         // animate()メソッドは迂闊に呼び出せないので、画像を描画するロジックをdraw()に分離した
         draw(step);
-    }
-
-    /**
-     * 画像を描画する。
-     *
-     * @param step アニメーションの進行状況
-     */
-    private void draw(int step) {
-        // 丸みを帯びた四角形を描くために、大きめの画像サイズを確保する
-        int width, height;
-        final int diameter = normalImg.getHeight();
-        final int radius = diameter / 2;
-        width = normalImg.getWidth() + diameter;
-        height = normalImg.getHeight();
-        GreenfootImage img = new GreenfootImage(width, height);
-        Graphics2D g = img.getAwtImage().createGraphics();
-
-        // 左右が丸みを帯びた四角形を描く
-        g.setColor(getColor(step));
-        g.fillOval(0, 0, diameter, diameter);
-        g.fillOval(width - diameter, 0, diameter, diameter);
-        g.fillRect(radius, 0, width - diameter, height);
-        g.dispose();
-
-        // 通常時の画像を重ねて描画する
-        img.drawImage(normalImg, radius, 0);
-        Utils.updateMaxAlpha(img, maxAlpha);
-        setImage(img);
-    }
-
-    private void draw() {
-        draw(0);
-    }
-
-    /**
-     * 現在のアニメーションの状態に合わせた、ボタンの背景にある丸みを帯びた四角形の色を返す
-     *
-     * @return
-     */
-    private Color getColor(int step) {
-        int r, g, b;
-        if (isMouseHovering) {
-            // normal -> hovering
-            r = normalColor.getRed() * (ANIME_STEP - step) + hoveringColor.getRed() * step;
-            g = normalColor.getGreen() * (ANIME_STEP - step) + hoveringColor.getGreen() * step;
-            b = normalColor.getBlue() * (ANIME_STEP - step) + hoveringColor.getBlue() * step;
-        } else {
-            // hovering -> normal
-            r = normalColor.getRed() * step + hoveringColor.getRed() * (ANIME_STEP - step);
-            g = normalColor.getGreen() * step + hoveringColor.getGreen() * (ANIME_STEP - step);
-            b = normalColor.getBlue() * step + hoveringColor.getBlue() * (ANIME_STEP - step);
-        }
-        r /= ANIME_STEP;
-        g /= ANIME_STEP;
-        b /= ANIME_STEP;
-        return new Color(r, g, b);
-    }
-
-    @Override
-    public void onMouseIn(MouseInfo mouse) {
-        super.onMouseIn(mouse);
-        isMouseHovering = true;
-        startAnimation();
-    }
-
-    @Override
-    public void onMouseOut(MouseInfo mouse) {
-        super.onMouseOut(mouse);
-        isMouseHovering = false;
-        startAnimation();
     }
 }
