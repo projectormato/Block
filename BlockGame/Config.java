de2234ec981f7e1f67a432a6e2f7b2363df096f1
yuuki <yuuki0xff@gmail.com>
2016-07-25 21:40:00 +0900

    fixup! タイトルの背景画像を暗くした

--- BlockGame/Config.java
+++ BlockGame/Config.java
@@ -76,7 +76,7 @@ final public class Config {
         // リサイズしてから回転を行う。
         {null, StartWorld.class, "rogo", "rogo/title2.png", 0, 0, 0},
         {null, StartWorld.class, "bg", "bg/titleBG4.png", WORLD_WIDTH, WORLD_HEIGHT, 0},
-        {null, StageSelectWorld.class, "bg", "bg/titleBG4.jpg", WORLD_WIDTH, WORLD_HEIGHT, 0},
+        {null, StageSelectWorld.class, "bg", "bg/titleBG4.png", WORLD_WIDTH, WORLD_HEIGHT, 0},
         {null, PlayWorld.class, "bg", "bg/space.jpg", 0, 0, 0},
         {BaseWorld.class, Ball.class, "bg", "ball/r2.png", 40, 40, 0},
         {BaseWorld.class, Block.class, "bg", "block/b.png", 20, 20, 0},
