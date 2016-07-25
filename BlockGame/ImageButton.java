c80fd64ff8e23afbb4ea0b2a31577f84212d4e89
yuuki <yuuki0xff@gmail.com>
2016-07-26 01:01:45 +0900

    ImageButton: アニメーション完成

--- BlockGame/ImageButton.java
+++ BlockGame/ImageButton.java
@@ -12,7 +12,10 @@ import java.awt.Graphics2D;
  */
 public class ImageButton extends Button {
 
-    private boolean isMouseHovering = false;
+    // addedToWorldの描画で、背景がnormalColorで描画されるようにするため、trueにしている。
+    private boolean isMouseHovering = true;
+    private Color normalColor = Color.DARK_GRAY;
+    private Color hoveringColor = Color.GRAY;
 
     /**
      * keyに対応する画像のボタンを作成する。
@@ -28,14 +31,28 @@ public class ImageButton extends Button {
         super.addedToWorld(world);
 
         normalImg = Config.getImage(this, key);
-        setImage(normalImg);
+        draw();
     }
 
     @Override
     public void animate(int step) {
         super.animate(step);
+        if (step > ANIME_STEP) {
+            stopAnimation();
+            return;
+        }
         System.out.println("ImgBtn: update");
 
+        // animate()メソッドは迂闊に呼び出せないので、画像を描画するロジックをdraw()に分離した
+        draw(step);
+    }
+
+    /**
+     * 画像を描画する。
+     *
+     * @param step アニメーションの進行状況
+     */
+    private void draw(int step) {
         // 丸みを帯びた四角形を描くために、大きめの画像サイズを確保する
         int width, height;
         final int diameter = normalImg.getHeight();
@@ -46,7 +63,7 @@ public class ImageButton extends Button {
         Graphics2D g = img.getAwtImage().createGraphics();
 
         // 左右が丸みを帯びた四角形を描く
-        g.setColor(Color.DARK_GRAY);
+        g.setColor(getColor(step));
         g.fillOval(0, 0, diameter, diameter);
         g.fillOval(width - diameter, 0, diameter, diameter);
         g.fillRect(radius, 0, width - diameter, height);
@@ -56,6 +73,34 @@ public class ImageButton extends Button {
         img.drawImage(normalImg, radius, 0);
         Utils.updateMaxAlpha(img, maxAlpha);
         setImage(img);
+    }
+
+    private void draw() {
+        draw(0);
+    }
+
+    /**
+     * 現在のアニメーションの状態に合わせた、ボタンの背景にある丸みを帯びた四角形の色を返す
+     *
+     * @return
+     */
+    private Color getColor(int step) {
+        int r, g, b;
+        if (isMouseHovering) {
+            // normal -> hovering
+            r = normalColor.getRed() * (ANIME_STEP - step) + hoveringColor.getRed() * step;
+            g = normalColor.getGreen() * (ANIME_STEP - step) + hoveringColor.getGreen() * step;
+            b = normalColor.getBlue() * (ANIME_STEP - step) + hoveringColor.getBlue() * step;
+        } else {
+            // hovering -> normal
+            r = normalColor.getRed() * step + hoveringColor.getRed() * (ANIME_STEP - step);
+            g = normalColor.getGreen() * step + hoveringColor.getGreen() * (ANIME_STEP - step);
+            b = normalColor.getBlue() * step + hoveringColor.getBlue() * (ANIME_STEP - step);
+        }
+        r /= ANIME_STEP;
+        g /= ANIME_STEP;
+        b /= ANIME_STEP;
+        return new Color(r, g, b);
     }
 
     @Override
