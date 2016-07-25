7ca1617591ac62fceaf58eb7c9456cbeab8c4663
yuuki <yuuki0xff@gmail.com>
2016-07-25 23:16:58 +0900

    add BaseAnimationActor
    個々のActorで実装しているアニメーションを共通化することを目的としています。

---
+++ BlockGame/BaseAnimationActor.java
@@ -0,0 +1,61 @@
+
+/**
+ * アニメーションをするActor
+ * 
+ * アニメーションの実装に便利なメソッドを提供する。
+ *
+ * @author yuuki0xff
+ */
+public abstract class BaseAnimationActor extends BaseActor implements AnimationActor {
+
+    private int count = -1;
+    protected final int ANIME_SPEED = 5;
+    protected final int ANIME_STEP = 20;
+
+    @Override
+    public void tick() {
+        super.tick();
+
+        // アニメーションが停止しているとき
+        if (count < 0) {
+            return;
+        }
+
+        int step = count / ANIME_SPEED;
+        count++;
+        // フレームを間引きするべきとき
+        if (count % ANIME_SPEED != 0) {
+            return;
+        }
+
+        animate(step);
+    }
+
+    /**
+     * アニメーションを停止する
+     */
+    public void stopAnimation() {
+        count = -1;
+    }
+
+    /**
+     * アニメーションを開始する。
+     */
+    public void startAnimation() {
+        if (count < 0) {
+            count = 0;
+        }
+    }
+
+    public void resetAnimation() {
+        count = 0;
+    }
+
+    /**
+     * アニメーションが行われているときは、1フレームごとに呼び出される。 画像や位置の更新をここで行う。
+     *
+     * @param step アニメーションの進行状況
+     */
+    public void animate(int step) {
+    }
+}
